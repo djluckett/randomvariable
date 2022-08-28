4f00018e3b417307ee2611815c984212e8607093
Daniel Luckett <daniel.luckett.work@gmail.com>
2022-08-27 21:08:13 -0400

    Adding functions for mean and variance

---
+++ R/summary_metrics.R
@@ -0,0 +1,54 @@
+#' Calculate Expected Value
+#'
+#' This function calculates the expected value of an object.
+#'
+#' @param X An object.
+#'
+#' @return An object.
+#'
+#' @export
+expectedValue = function(X, ...) {
+  UseMethod("expectedValue", X)
+}
+
+#' Calculate Variance
+#'
+#' This function calculates the variance of an object.
+#'
+#' @param X An object.
+#'
+#' @return An object.
+#'
+#' @export
+variance = function(X, ...) {
+  UseMethod("variance", X)
+}
+
+#' Calculate Expected Value of A Random Variable
+#'
+#' This function calculates the expected value of a random variable.
+#'
+#' @param X An object of class "RV".
+#'
+#' @return A length one numeric, the expected value of the random variable X.
+#'
+#' @export
+expectedValue.RV = function(X) {
+  PDF = getPDF(X)
+  integrate(function(x) return(x * PDF(x)), lower = X[["lower"]], upper = X[["upper"]])[["value"]]
+}
+
+#' Calculate the Variance of A Random Variable
+#'
+#' This function calculates the variance of a random variable.
+#'
+#' @param X An object of class "RV".
+#'
+#' @return A length one numeric, the variance of the random variable X.
+#'
+#' @export
+variance.RV = function(X) {
+  PDF = getPDF(X)
+  mu = expectedValue(X)
+  integrate(function(x) return((x - mu)^2 * PDF(x)), lower = X[["lower"]], upper = X[["upper"]])[["value"]]
+}
